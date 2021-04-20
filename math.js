8e6692bc962aa09de14499a27e0e8dfa5205f17f
LAKKI <thakurluckysinghbrh@gmail.com>
2021-04-20 10:59:05 +0530

    commented service workers

--- math.js
+++ math.js
@@ -214,42 +214,44 @@ function toggleLevel() {
    dropdownState('levelSet', 'levelDropdownSymbol')
 }
 
-if ('serviceWorker' in navigator) {
-   window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/Math-Practice/sw.js').then(function (registration) {
-         // Registration was successful
-         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function (err) {
-         // registration failed :(
-         console.log('ServiceWorker registration failed: ', err);
-      });
-   });
-}
+//code related to service workers(for making app pwa) is commented out because it was not working
 
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
-window.addEventListener('beforeinstallprompt', (e) => {
-   // Prevent Chrome 67 and earlier from automatically showing the prompt
-   e.preventDefault();
-   // Stash the event so it can be triggered later.
-   deferredPrompt = e;
-   // Update UI to notify the user they can add to home screen
-   addBtn.style.display = 'block';
+// if ('serviceWorker' in navigator) {
+//    window.addEventListener('load', function () {
+//       navigator.serviceWorker.register('/Math-Practice/sw.js').then(function (registration) {
+//          // Registration was successful
+//          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }, function (err) {
+//          // registration failed :(
+//          console.log('ServiceWorker registration failed: ', err);
+//       });
+//    });
+// }
 
-   addBtn.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-         if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-         } else {
-            console.log('User dismissed the A2HS prompt');
-         }
-         deferredPrompt = null;
-      });
-   });
-});
+// let deferredPrompt;
+// const addBtn = document.querySelector('.add-button');
+// addBtn.style.display = 'none';
+// window.addEventListener('beforeinstallprompt', (e) => {
+//    // Prevent Chrome 67 and earlier from automatically showing the prompt
+//    e.preventDefault();
+//    // Stash the event so it can be triggered later.
+//    deferredPrompt = e;
+//    // Update UI to notify the user they can add to home screen
+//    addBtn.style.display = 'block';
+
+//    addBtn.addEventListener('click', (e) => {
+//       // hide our user interface that shows our A2HS button
+//       addBtn.style.display = 'none';
+//       // Show the prompt
+//       deferredPrompt.prompt();
+//       // Wait for the user to respond to the prompt
+//       deferredPrompt.userChoice.then((choiceResult) => {
+//          if (choiceResult.outcome === 'accepted') {
+//             console.log('User accepted the A2HS prompt');
+//          } else {
+//             console.log('User dismissed the A2HS prompt');
+//          }
+//          deferredPrompt = null;
+//       });
+//    });
+// });
